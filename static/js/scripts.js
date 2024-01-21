1ef3a88a0c402a5afb3b8221e1dfae86fa8bd037
marciofmarcelino <2105256@aluno.univesp.br>
2024-01-21 13:03:04 -0300

    add blur

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,18 +2,49 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuIcon = document.getElementById('menu-icon');
     const closeIcon = document.getElementById('close-icon');
     const menu = document.getElementById('menu');
+    
   
     menuIcon.addEventListener('click', function () {
+      aplicarBlur();
       menu.style.display = 'block';
       menuIcon.style.display = 'none';
       closeIcon.style.display = 'block';
-      
+     
     });
   
     closeIcon.addEventListener('click', function () {
+      removerBlur();
       menu.style.display = 'none';
       menuIcon.style.display = 'block';
       closeIcon.style.display = 'none';
     });
   });
   
+
+function aplicarBlur() {
+  var content = document.getElementsByClassName('container');
+  var excecao = document.querySelector('.header');
+
+  if (content.length > 0) {
+    
+    for (var i=0; i<content.length; i++) {
+      if (content[i] !== excecao) {
+        content[i].style.filter = 'blur(5px)';
+      }
+      
+    }
+  }
+  
+};
+
+function removerBlur(){
+  var content = document.getElementsByClassName('container');
+
+  if (content.length > 0) {
+
+    for (var i=0; i<content.length; i++) {
+      content[i].style.filter = 'none';
+    }
+  }
+
+};
